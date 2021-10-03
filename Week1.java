0808aff11058d7dd7b5d3446fec643a4eb26342e
indrakarunas <91792983+indrakarunas@users.noreply.github.com>
2021-10-03 13:49:44 +0530

    Add files via upload

---
+++ Week1.java
@@ -0,0 +1,17 @@
+package Maven;
+
+public class Week1 {
+
+	public static void main(String[] args) {
+		
+	
+	int number=5;
+	if(number%2==0) {
+		System.out.println("It is a prime number");
+		}else
+			
+			System.out.println("It is not  a prime number");
+	      
+	}
+
+}
